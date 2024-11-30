a9179dd15337c71ed56c3490fc423e697232a908
Adefila Joshua <adefila44@gmail.com>
2024-11-30 21:22:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let menuIcon = document.querySelector(".nav-bars");
+let mobileNav = document.querySelector(".mobile-nav")
+
+menuIcon.addEventListener("click", function(){
+    mobileNav.style.display = "flex";
+})
