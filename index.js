7b5e7f8fbdb16306152326912976a70ad4059bb5
Adefila Joshua <adefila44@gmail.com>
2024-12-17 14:19:17 +0100

    js commit

--- index.js
+++ index.js
@@ -7,15 +7,14 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const navBars = document.querySelector('.nav-bars'); // Hamburger icon
-    const mobileNav = document.querySelector('.mobile-nav'); // Mobile nav container
+    const navBars = document.querySelector('.nav-bars'); 
+    const mobileNav = document.querySelector('.mobile-nav');
 
     // Toggle mobile navigation on hamburger click
     navBars.addEventListener('click', () => {
-        mobileNav.classList.toggle('nav-active'); // Add or remove the nav-active class
+        mobileNav.classList.toggle('nav-active'); 
     });
 
-    // Optional: Add functionality to close the menu when clicking outside
     document.addEventListener('click', (event) => {
         if (!mobileNav.contains(event.target) && !navBars.contains(event.target)) {
             mobileNav.classList.remove('nav-active');
